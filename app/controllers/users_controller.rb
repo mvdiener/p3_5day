eed87a4cf1bd447d8a405872474f607622d9ba5d
Andrew Adams & Maximus Prime <pair+andrewjadams3.mvdiener@devbootcamp.com>
2014-08-07 16:30:23 -0500

    Cleaned up sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,29 @@
 class UsersController < ApplicationController
 	def index
+		current_user
 		@users = User.all
 	end
 
 	def new
-		@user = User.new
+		if current_user
+			redirect_to(root_path)
+		else
+			@user = User.new
+			render 'new'
+		end
 	end
 
 	def create
 		@user = User.new(user_params)
 		if @user.save
-			session[:user_id] = @user.id
-			redirect_to posts_path
+			session[:current_user_id] = @user.id
+			redirect_to root_path
 		else
 			render 'new'
 		end
 	end
 
-	private 
+	private
 
 	   def user_params
      	params.require(:user).permit(:username, :email, :password,
