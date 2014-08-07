792a8e2bcb461f4085fcae884281ce242515f7c9
Jonathan Young & Timothy Howard <pair+jyoung217.tim2819@devbootcamp.com>
2014-08-07 15:43:26 -0500

    try 3

--- spec/views/sessions/new.html.erb_spec.rb
+++ spec/views/sessions/new.html.erb_spec.rb
@@ -6,13 +6,6 @@ require 'rails_helper'
 
 describe "New pages" do
 
-	subject { page }
-
-	describe "signup page" do
-		before { visit posts_path }
-
-		it { should have_content('Sign up')}
-		it { should have_content('Log in')}
-	end
+	
 
 end
