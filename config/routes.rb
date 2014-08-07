ff9c7e3b9559a200cf158f9bd6a6368c1f497e1e
Apprentice <mvdiener@gmail.com>
2014-08-07 09:44:51 -0500

    added index homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :users, :posts
   resources :sessions, only: [:new, :create, :destroy]
+  root 'posts#index'
 
   match '/signup', to: 'users#new', via: 'get'
   match '/login', to: 'sessions#new', via: 'get'
   match '/logout', to: 'sessions#destroy', via: 'delete'
-  root 'posts#index'
 end
