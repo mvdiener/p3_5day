37898429c421c09084c999f4263b6e862c940a64
Andrew Adams & Maximus Prime <pair+andrewjadams3.mvdiener@devbootcamp.com>
2014-08-10 13:58:29 -0500

    Work on airline flight parsing for json

--- app/controllers/airlines_controller.rb
+++ app/controllers/airlines_controller.rb
@@ -3,6 +3,7 @@ class AirlinesController < ApplicationController
 	def show
 		current_user
 		@airline = Airline.find(params[:id])
+		@flights = @airline.parse_flights
 	end
 
 end
--- app/models/airline.rb
+++ app/models/airline.rb
@@ -10,4 +10,27 @@ class Airline < ActiveRecord::Base
 		satisfied = posts.select{|post| post.satisfied}
 		satisfied.length/total.to_f * 100
 	end
+
+	def parse_flights
+		self.flights.map do |flight|
+			{
+				"flight" => flight.flight_number,
+				"arrivalDifference" => arrival_difference(flight),
+				"departureDifference" => departure_difference(flight)
+			}
+		end
+	end
+
+	def difference_to_s(time)
+		minutes = (time/60).to_i.to_s
+	end
+
+	def arrival_difference(flight)
+		difference_to_s(flight.arrival_actual - flight.arrival_scheduled)
+	end
+
+	def departure_difference(flight)
+		difference_to_s(flight.departure_actual - flight.departure_scheduled)
+	end
+
 end
